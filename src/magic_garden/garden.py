992ea1e253247215a7f998db906fd4b1ad99c391
Kyle Oliver <oliverke@mail.uc.edu>
2026-05-14 01:55:09 -0400

    feat: improve garden homing to work consistently

--- src/magic_garden/garden.py
+++ src/magic_garden/garden.py
@@ -2,7 +2,6 @@
 import time
 
 from magic_garden._types import GameDirection
-from magic_garden.interact import hold_key
 from magic_garden.interact import move_character
 from magic_garden.interact import teleport_to_garden
 
@@ -12,14 +11,5 @@ def home_garden_position() -> None:
     for _ in range(12):
         teleport_to_garden()
         move_character(direction=GameDirection.LEFT, amount=2)
-        move_character(direction=GameDirection.UP)
-
-
-if __name__ == "__main__":
-    time.sleep(5)
-    for _ in range(12):
-        move_character(direction=GameDirection.LEFT, amount=20)
-        move_character(direction=GameDirection.UP)
-        move_character(direction=GameDirection.RIGHT, amount=20)
-        move_character(direction=GameDirection.DOWN)
-
+        teleport_to_garden()
+        move_character(direction=GameDirection.UP, amount=2)
